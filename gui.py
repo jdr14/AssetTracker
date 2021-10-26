b9c0d6091f5b616de474639f13eb87a7fbea0d72
jdr14 <jdr14@vt.edu>
2021-10-26 02:27:51 -0400

    Created a seperate gui testing file.  Added a working push button to gui.py

--- gui.py
+++ gui.py
@@ -2,17 +2,21 @@ import os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
+from exceptions import *
 
-# with open("data.txt", 'w+') as f:
+# with open('data.txt', 'w+') as f:
 #     for item in dir(QMainWindow):
 #         f.write("\n{}".format(item))
 
+def _checkCoordinateTuple(tp, desired_length=4):
+    return (tp and type(tp) == tuple and len(tp) == desired_length)
+
 class Window(QMainWindow):
     def __init__(self, window_xywh=None, window_title="", window_icon=None):
-        super().__init__()
+        super(Window, self).__init__()
         
         # Set window size/coordinates
-        if window_xywh:
+        if _checkCoordinateTuple(window_xywh):
             self.setGeometry(*window_xywh)
         else: # default setup
             self.setGeometry(400, 200, 900, 600) # x, y, w, h
@@ -27,5 +31,22 @@ class Window(QMainWindow):
         self.show()
 
 
-# class Button(QWidget):
-#     def __init__()
+class PushButton(QPushButton):
+    def __init__(self, button_xywh=None, button_text=None, button_icon=None, parent=None):
+        super(PushButton, self).__init__(parent=parent)
+        self.button_icon = button_icon
+        self.button_text = button_text
+        self.button_xywh = button_xywh
+        self.parent = parent
+        self.setup()
+        self.show()
+
+    def setup(self) -> None:
+        self.setIcon(QIcon(self.button_icon))
+        if self.button_text and type(self.button_text) == str:
+            self.setText(self.button_text)
+        if _checkCoordinateTuple(self.button_xywh):
+            self.setGeometry(*self.button_xywh)
+        self.setShortcut('+')
+        self.clicked.connect(self.close)
+        self.setToolTip("Add a record")
