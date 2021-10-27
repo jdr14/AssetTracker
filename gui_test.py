fa80ce709a0080c3411a8ee4e663d0cdeb120298
jdr14 <jdr14@vt.edu>
2021-10-27 11:23:25 -0400

    Was able to get added record to show on the scrollview

--- gui_test.py
+++ gui_test.py
@@ -2,13 +2,13 @@ import sys
 from copy import deepcopy
 from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QDialogButtonBox
 from PyQt5.QtWidgets import QLineEdit, QVBoxLayout, QLabel, QGridLayout, QPushButton
-from PyQt5.QtWidgets import QScrollArea, QSizePolicy, QGroupBox
+from PyQt5.QtWidgets import QScrollArea, QSizePolicy, QGroupBox, QTableWidget
 from PyQt5.QtGui import QIcon, QPalette, QFont
 from PyQt5.QtCore import Qt
 from gui import *
 
 # class Record:
-# 	def __init__(id, name, price, quantity):
+#   def __init__(id, name, price, quantity):
 
 def main():
     app = QApplication(sys.argv)
@@ -76,41 +76,43 @@ def main():
     scrollview = QScrollArea()
     scrollview.setStyleSheet("background: tan")
 
+    svlayout = QVBoxLayout()
+    scrollview.setLayout(svlayout)
+    scrollview.setAlignment(Qt.AlignTop)
+
     # Define a slot to connect to once button is clicked
     def addRecord():
-    	print("{}".format((
-    		textEntrySetNum.text(), 
-    		textEntryName.text(), 
-    		textEntryPrice.text(), 
-    		textEntryQuantity.text()
-    	)))
+        print("{}".format((
+            textEntrySetNum.text(), 
+            textEntryName.text(), 
+            textEntryPrice.text(), 
+            textEntryQuantity.text()
+        )))
+        record = QTableWidget(0,5)
+        record.setShowGrid(False)
+        record.setHorizontalHeaderLabels([
+            textEntryID.text(),
+            textEntrySetNum.text(), 
+            textEntryName.text(), 
+            textEntryPrice.text(), 
+            textEntryQuantity.text()
+        ])
+        record.setFixedHeight(20)
+        svlayout.addWidget(record)
         # scrollview.add
     
     def deleteRecord():
         print("Delete record")
 
 
-    # Create an add button
+    # Create an add button and add some styling for the button
     addButton = QPushButton()
-    addButton.setGeometry(
-    	    window.width()  - (window.width() / 10), 
-            (window.height() / 10), 
-            40, 
-            40
-    )
-    # Add some styling for the button
     addButton.setText("Add Record")
     addButton.setIcon(QIcon("images/google_icons/outline_add_black_24dp.png"))
     addButton.clicked.connect(addRecord)
     addButton.setStyleSheet("border: 2px solid black; background-color: #64ed32")
     
     deleteButton = QPushButton()
-    deleteButton.setGeometry(
-            window.width()  - (window.width() / 10), 
-            (window.height() / 10), 
-            40, 
-            40
-    )
     deleteButton.setText("Delete Record")
     deleteButton.setIcon(QIcon("images/google_icons/outline_remove_black_24dp.png"))
     deleteButton.clicked.connect(deleteRecord)
@@ -118,7 +120,7 @@ def main():
 
     # Create a grid which will be the parent layout for all widgets
     grid = QGridLayout()
-    grid.setSpacing(20)
+    grid.setSpacing(15)
     grid.addWidget(headingGroupBox,   0, 0, 2, 6)
     grid.addWidget(addButton, 0, 6)         # add button
     grid.addWidget(deleteButton, 1, 6)      # delete button
@@ -130,4 +132,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+    main()
