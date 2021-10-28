ec7154c3b788a36a0d9e4781dbc0fb8e095572c2
jdr14 <jdr14@vt.edu>
2021-10-28 11:18:01 -0400

    Separated the main in authenticated_request out into logical functions to aid with proper encapsulation.

--- authenticated_request.py
+++ authenticated_request.py
@@ -12,30 +12,32 @@ from generate_encrypted_keys import read_encrypted_keys_from_file, read_decrypte
 
 API_BASE_URL = "https://api.bricklink.com/api/store/v1"
 
-fernet_key = generate_fernet_key() # generate a fernet key
-encrypt_keys(fernet_key) # encrypt the oauth keys with the fernet key
-write_encrypted_keys_to_file() # write the keys to the file (binary)
+def createSession():
 
-encrypted_list = read_encrypted_keys_from_file()
-ENCRYPTED_CONSUMER_KEY    = encrypted_list[0].strip(str.encode('\n'))
-ENCRYPTED_CONSUMER_SECRET = encrypted_list[1].strip(str.encode('\n'))
-ENCRYPTED_TOKEN_VALUE     = encrypted_list[2].strip(str.encode('\n'))
-ENCRYPTED_TOKEN_SECRET    = encrypted_list[3].strip(str.encode('\n'))
+    fernet_key = generate_fernet_key() # generate a fernet key
+    encrypt_keys(fernet_key) # encrypt the oauth keys with the fernet key
+    write_encrypted_keys_to_file() # write the keys to the file (binary)
 
-def main():
+    encrypted_list = read_encrypted_keys_from_file()
+    ENCRYPTED_CONSUMER_KEY    = encrypted_list[0].strip(str.encode('\n'))
+    ENCRYPTED_CONSUMER_SECRET = encrypted_list[1].strip(str.encode('\n'))
+    ENCRYPTED_TOKEN_VALUE     = encrypted_list[2].strip(str.encode('\n'))
+    ENCRYPTED_TOKEN_SECRET    = encrypted_list[3].strip(str.encode('\n'))
+
     _consumer_key    = str(fernet_key.decrypt(ENCRYPTED_CONSUMER_KEY).decode())
     _consumer_secret = str(fernet_key.decrypt(ENCRYPTED_CONSUMER_SECRET).decode())
     _token_value     = str(fernet_key.decrypt(ENCRYPTED_TOKEN_VALUE).decode())
     _token_secret    = str(fernet_key.decrypt(ENCRYPTED_TOKEN_SECRET).decode())
 
     # Create a session
-    session = OAuth1Session(
+    return OAuth1Session(
         client_key            = _consumer_key,
         client_secret         = _consumer_secret,
         resource_owner_key    = _token_value,
         resource_owner_secret = _token_secret,
     )
 
+def getRequest(session, item_num, item_type = "set"):
     query_options = {
         'guide_type': 'sold',  # stock = current items for sale, sold = last 6 months sales
         'new_or_used': 'N',    # N = new, U = used
@@ -43,18 +45,20 @@ def main():
         'currency_code': 'USD' # US dollars
     }
     
-    set_num = 75255
-    item_type = "set"
     request = "{0}/items/{1}/{2}-1/price?{3}={4}&{5}={6}&{7}={8}&{9}={10}".format(
-        API_BASE_URL, 
+        API_BASE_URL,
         item_type,
-        set_num,
+        item_num,
         'guide_type',    query_options['guide_type'],
         'new_or_used',   query_options['new_or_used'],
         'country_code',  query_options['country_code'],
         'currency_code', query_options['currency_code'],
     )
-    response = session.get(request)
+    return session.get(request)
+
+def main():
+    session = createSession()
+    response = getRequest(session, 75255)
 
     if (response.ok == True):
         json_response = response.json()
