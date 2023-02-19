7f2b878e7eb6ca7632d264be61731fa00949309c
jdr14 <jdr14@vt.edu>
2023-02-18 19:53:57 -0500

    Code cleanup, removed old debug statements and unnecessary whitespace/newlines.

--- server.py
+++ server.py
@@ -4,71 +4,38 @@ from re import L
 from flask import Flask
 from requests.api import request  
 from flask_restful import reqparse
-# from authenticated_request import createSession, getRequest, API_BASE_URL
-
 from requests_oauthlib import OAuth1, OAuth1Session
-
 from session import Session, API_TYPE
 
 app = Flask(__name__) # name for the Flask app (refer to output)
-
 bricklink_session = Session(API_TYPE.BRICKLINK) 
 
-
-# Define the index route
+# Define the index route default home route (home of web UI)
 @app.route("/")
 def index():
-    
-    # default home route
-    # home of web UI
-
     return "<h1>Hello from Flask!</h1>"
-
-
 
 @app.route("/API/bricklink", methods=['GET']) # this route accepts get request from localhost:5000/API/bricklink?set=<setnumber>
 def makeAPIReq():
-
-    
-
-    #grab REST variables from client request
+    # grab REST variables from client request
     parser = reqparse.RequestParser()
     parser.add_argument('set', type=str)
     args = parser.parse_args()
     set_num = args['set']
-
     print("set num: {}".format(set_num))
-
-    # bricklink_response = getRequest(BRICKLINK_SESSION, set_num)
     bricklink_response = bricklink_session.getRequest(set_num)
 
-# make bricklink request
-    
+    # make bricklink request
     if (bricklink_response.ok == True):
         bricklink_response = bricklink_response.json()
-        # print(bricklink_response)
         return bricklink_response #" Average price = $ {}".format(bricklink_response['data']['avg_price'])
-
     else:
         print(str(bricklink_response.status_code) + " " + bricklink_response.reason)
-        
-    
     return ""
-
-    
-
-    
     # respond to client
     # http://localhost:5000/API/bricklink?set=75255
-    
-    
-
-
-    
 
 
 # Run Flask if the __name__ variable is equal to __main__
 if __name__ == "__main__":
-
-
     app.run()
